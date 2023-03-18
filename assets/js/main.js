18a6b899e9c82991c7cb43964eb4f45722d10eed
Thomas Joseph Cottis <92066036+Thomas-Cottis-21@users.noreply.github.com>
2023-03-17 22:00:28 -0600

    updated navbars in all pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ const navBarElements = document.getElementsByClassName("navbar-element");
 const navBarFrame = document.getElementById("nav-bar-frame");
 const navBarMobileIcon = document.getElementById("nav-bar-mobile-icon");
 
+const navBarDim = navBar.clientHeight;
+
 /* RGBA variable */
 let R = "93";
 let G = "109";
@@ -31,21 +33,25 @@ document.addEventListener("scroll", navBarScroll);
 
 /* mobile nav menu */
 
+/* Plan on using session storage in order to run the mobile nav bar event handlers. Rewriting this logic would be necessary, but it would be less complicated */
+
 window.addEventListener("resize", (event) => {
     const width = window.innerWidth;
     console.log(width);
     navBarMobile(width);
 })
-
 window.addEventListener("load", (event) => {
     const width = window.innerWidth;
     console.log(width);
     navBarMobile(width);
 })
 
+
 const navBarMobile = (element) => {
     if (Math.floor(element) <= 700) {
         navBarMobileIcon.style.display = "inline";
+        navBarMobileIcon.style.width = navBarDim + "px";
+        navBarMobileIcon.style.height = navBarDim + "px";
         for (let i = 0; i < navBarElements.length; i++) {
             navBarElements[i].style.display = "none";
         }
